1c6204e2657d289d1cb0389f7b3bc837fbbeaeb9
Kantaro0829 <eshangwan@gmail.com>
2023-12-17 23:14:45 +0900

    add main function

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+def main():
+    # Load environment variables from .env file
+    load_dotenv()
 
-# Access environment variables
-api_key = os.getenv('API_KEY')
-print(api_key)
+    # Access environment variables
+    api_key = os.getenv('API_KEY')
+    print(api_key)
+    
+if __name__ == "__main__":
+   main()
