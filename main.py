2032706360a9ce36a4595f93811b57cd8a7bb966
Kantaro0829 <eshangwan@gmail.com>
2023-12-18 00:06:28 +0900

    request siimple question to gemini-pro

--- main.py
+++ main.py
@@ -6,8 +6,21 @@ def setup(api_key : str) -> genai:
     genai.configure(api_key=api_key)
     return genai
 
-def print_available_medels(genemi : genai):
-    for m in genemi.list_models():
+def choose_model(gemini : genai, model_name : str):
+    model = gemini.GenerativeModel(model_name)
+    return model
+
+def get_answer(model : genai, question : str, is_stream) -> str:
+    if is_stream:
+        response = model.generate_content(question, stream=True)
+        return response.text
+    
+    response = model.generate_content(question)
+    return response.text
+
+
+def print_available_medels(gemini : genai):
+    for m in gemini.list_models():
         if 'generateContent' in m.supported_generation_methods:
             print(m.name)
 
@@ -18,8 +31,12 @@ def main():
 
     # Access environment variables
     api_key = os.getenv('API_KEY')
-    genemi = setup(api_key=api_key)
-    print_available_medels(genemi=genemi)
+    gemini = setup(api_key=api_key)
+    print_available_medels(gemini=gemini)
+    model = choose_model(gemini=gemini, model_name='gemini-pro')
+    answer = get_answer(model=model, question="What is the meaning of life?", is_stream=False)
+    print(answer)
+
     
 if __name__ == "__main__":
    main()
