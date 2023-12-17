0ac588bb67dd228bee54a4dc554e9c262da26f43
Kantaro0829 <eshangwan@gmail.com>
2023-12-17 23:41:48 +0900

    print available models of Genemi

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import os
 from dotenv import load_dotenv
+import google.generativeai as genai
+
+def setup(api_key : str) -> genai: 
+    genai.configure(api_key=api_key)
+    return genai
+
+def print_available_medels(genemi : genai):
+    for m in genemi.list_models():
+        if 'generateContent' in m.supported_generation_methods:
+            print(m.name)
+
 
 def main():
     # Load environment variables from .env file
@@ -7,7 +18,8 @@ def main():
 
     # Access environment variables
     api_key = os.getenv('API_KEY')
-    print(api_key)
+    genemi = setup(api_key=api_key)
+    print_available_medels(genemi=genemi)
     
 if __name__ == "__main__":
    main()
